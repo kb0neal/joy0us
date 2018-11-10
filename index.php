96111c11e0296af29e38c6443a88c5a20faa01ff
Kyle O’Neal <kboneal1994@gmail.com>
2018-11-09 22:31:44 -0500

    1

---
+++ index.php
@@ -0,0 +1,44 @@
+<!doctype html> <!—- FROM GITHUB —>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src='require.js'></script>
+    
+<?php
+file_put_contents("chatur.json", fopen("https://chaturbate.com/statsapi/?username=kc0untry777&token=KoPJJrv7kQu0mTq4xJnBLO02","r"));
+?>
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  	<nav class="navbar navbar-light bg-light">
+  		<span class="navbar-brand mb-0 h1">Chaturbate</span>
+	</nav>
+
+    <h1 id='usrname'>Hello, world!</h1>
+    <script>
+const obj;
+$.getJSON("chatur.json", function(json) {
+    obj = json;
+});
+document.write(obj + obj.username)
+</script>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <?php
+    echo '<script>';
+    echo '';
+    echo '</script>';
+    ?>
+    
+  </body>
+</html>
